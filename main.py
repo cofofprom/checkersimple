71735b860d56e6283ea856b6cfa466fbdee0ed8e
cofofprom <nightmarelandscape@gmail.com>
2021-12-06 20:34:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ class Resp(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_response()
         self.wfile.write("{'hello!'}".encode("utf-8"))
-
+        
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        pfile = open("tt.py", "w")
+        pfile.write(post_data)
+        outfile = open("result.txt", "w")
+        subprocess.run("python3 tt.py", stdout=outfile)
+        self.wfile.write(outfile.read(1024))
+        pfile.close()
+        outfile.close()
+        
 server_address = ("", int(os.environ["PORT"]))
 print(server_address)
 h = http.server.HTTPServer(server_address, Resp)
